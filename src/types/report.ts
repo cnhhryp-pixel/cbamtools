cb435a8b0b151998da6e933c3586c9ee497c8cc7
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-19 23:19:20 +0800

    Add CBAM report data structure types

---
+++ src/types/report.ts
@@ -0,0 +1,11 @@
+export interface CBAMReportData {
+ companyName:string;
+ email:string;
+ productName:string;
+ hsCode:string;
+ originCountry:string;
+ quantity:number;
+ emissionFactor:number;
+ carbonPrice:number;
+ estimatedCost:number;
+}
