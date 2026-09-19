b29eb5d371466b03572abeee4d59c292378e10a9
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-20 07:46:59 +0800

    Fix homepage component imports for Cloudflare build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import HomeHero from "@/components/HomeHero";
-import ToolCards from "@/components/ToolCards";
-import TrustSection from "@/components/TrustSection";
-import IndustriesSection from "@/components/IndustriesSection";
-import PopularResources from "@/components/PopularResources";
-import ProfessionalReportCTA from "@/components/ProfessionalReportCTA";
-import HomeFAQ from "@/components/HomeFAQ";
-import CBAMCTA from "@/components/CBAMCTA";
+import HomeHero from "../components/HomeHero";
+import ToolCards from "../components/ToolCards";
+import TrustSection from "../components/TrustSection";
+import IndustriesSection from "../components/IndustriesSection";
+import PopularResources from "../components/PopularResources";
+import ProfessionalReportCTA from "../components/ProfessionalReportCTA";
+import HomeFAQ from "../components/HomeFAQ";
+import CBAMCTA from "../components/CBAMCTA";
 
 export default function Home() {
   return (
