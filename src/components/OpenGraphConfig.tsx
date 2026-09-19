118e589199b2fff14ee18712d6770b3fcfd57974
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-19 22:50:41 +0800

    Add OpenGraph metadata component

---
+++ src/components/OpenGraphConfig.tsx
@@ -0,0 +1,7 @@
+export const openGraphConfig={
+ title:'CBAMtools - CBAM Calculator and Compliance Tools',
+ description:'Calculate CBAM costs, check HS codes and generate CBAM assessment reports.',
+ url:'https://cbamtools.com',
+ siteName:'CBAMtools',
+ type:'website'
+};
