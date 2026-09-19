ab2bdfadf0bae3141fe566f51b5a02d3fc19326f
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-19 21:40:34 +0800

    Add printable CBAM report button

---
+++ src/components/PrintReportButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function PrintReportButton() {
+  function printReport() {
+    window.print();
+  }
+
+  return (
+    <button
+      onClick={printReport}
+      className="rounded-lg bg-blue-600 px-6 py-3 text-white"
+    >
+      Print CBAM Report
+    </button>
+  );
+}
