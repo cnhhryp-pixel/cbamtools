0f98aaf2e0e5e46a97bda2d36ea1af38a00b5542
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-26 11:24:21 +0800

    Set Professional Report commercial config to €49

---
+++ src/lib/commercial.ts
@@ -0,0 +1,5 @@
+export const PROFESSIONAL_REPORT_PRICE_EUR = 49;
+export const PROFESSIONAL_REPORT_AMOUNT = "49.00";
+export const PROFESSIONAL_REPORT_CURRENCY = "EUR";
+export const PROFESSIONAL_REPORT_PRICE_LABEL = "€49";
+export const PAYMENT_API_BASE = "https://pay.cbamtools.com";
