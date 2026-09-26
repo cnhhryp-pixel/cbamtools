6ac4841234c3efb0c1721c1fe6d20ca3936b8b02
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-26 09:58:26 +0800

    Update CBAMTools support email

--- src/components/OrganizationSchema.tsx
+++ src/components/OrganizationSchema.tsx
@@ -4,7 +4,7 @@ export default function OrganizationSchema(){
     "@type":"Organization",
     name:"CBAMTools",
     url:"https://cbamtools.com/",
-    email:"cnhhryp@gmail.com",
+    email:"sales@cbamtools.com",
     description:"Practical CBAM calculation, product-screening and assessment workflow tools."
   };
   return <script type="application/ld+json" dangerouslySetInnerHTML={{__html:JSON.stringify(schema)}}/>;
