5f35cdc366f499e5041ab04ed966877d6b1d2ca1
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-26 13:35:29 +0800

    Remove legacy automatic payment and $9.90 component code

--- src/components/PayPalReportButton.tsx
+++ src/components/PayPalReportButton.tsx
@@ -1,1 +1,3 @@
-export default function PayPalReportButton(){return <a href="/checkout" className="inline-block rounded-lg bg-blue-600 px-5 py-3 text-white">Continue to PayPal checkout - $9.90</a>}
+export default function PayPalReportButton(){
+  return <a href="/checkout" className="inline-block rounded-lg bg-blue-600 px-5 py-3 text-white">Continue to €49 PayPal checkout</a>;
+}
